85ae7277d55ad7ea2c97e0e606ace0e1b7be11f4
merraculous <bella434@gmail.com>
2025-01-07 11:59:20 -0800

    Basic moveability for Alien Invasion

--- chapter_12/alien_invasion.py
+++ chapter_12/alien_invasion.py
@@ -5,27 +5,100 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 
-def run_game():
+import sys
+
+class AlienInvasion:
+
     # Initialize pygame, settings, and screen object.
-    pygame.init()
-    ai_settings = Settings()
-    screen = pygame.display.set_mode(
-        (ai_settings.screen_width, ai_settings.screen_height))
-    pygame.display.set_caption("Alien Invasion")
-    
-    # Set the background color.
-    bg_color = (230, 230, 230)
-    
-    # Make a ship.
-    ship = Ship(ai_settings, screen)
-    # Make a group to store bullets in.
-    bullets = Group()
+    #   Note: PCC show how to make game window fullscreen. On my own 
+    #   system, did not work well or better so for this project, 
+    #   window will remain defined in settings.py
+    #       -may return to later
+    def __init__(self):
+        
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
 
-    # Start the main loop for the game.
-    while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        self.ship = Ship(self.settings, self.screen)
+        
+    
 
-run_game()
+    def run_game(self):
+        '''       
+        # Make a group to store bullets in.
+        bullets = Group()
+        '''
+
+        # Start the main loop for the game.
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+            self.clock.tick(60)
+            
+            
+            '''
+            gf.check_events(ai_settings, self.screen, ship, bullets)
+            ship.update()
+            gf.update_bullets(bullets)
+            gf.update_screen(ai_settings, self.screen, ship, bullets)
+            '''
+            
+    def _check_events(self):
+    
+        # respond to key events or mouse presses.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+                
+            # if a key is pressed
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+                    
+            # if a key is released
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+                
+                    
+    def _check_keydown_events(self, event):
+        
+        # ship moves
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+            
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+            
+        elif event.key == pygame.K_q:
+            sys.exit()
+            
+            
+    def _check_keyup_events(self, event):
+        
+        # ship stops moving
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+            
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+            
+            
+    def _update_screen(self):
+        
+        # Redraw screen during each pass of loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    #make game instance and run game.
+    ai = AlienInvasion()
+    ai.run_game()
--- chapter_12/settings.py
+++ chapter_12/settings.py
@@ -4,9 +4,9 @@ class Settings():
     def __init__(self):
         """Initialize the game's settings."""
         # Screen settings.
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.screen_width = 1100
+        self.screen_height = 600
+        self.bg_color = (30, 230, 230)
 
         # Ship settings.
         self.ship_speed_factor = 1.5
